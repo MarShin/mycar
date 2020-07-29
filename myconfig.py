cf5a1033d2d2366374328e96232042abce39851d
MarShin <martinshin95@gmail.com>
2020-07-29 16:31:17 +0800

    adjust reverse pwm

--- myconfig.py
+++ myconfig.py
@@ -64,9 +64,9 @@ STEERING_RIGHT_PWM = 290  # pwm value for full right steering, suggested 290
 #
 # #THROTTLE
 THROTTLE_CHANNEL = 0  # channel on the 9685 pwm board 0-15
-THROTTLE_FORWARD_PWM = 450  # pwm value for max forward throttle
+THROTTLE_FORWARD_PWM = 400  # pwm value for max forward throttle
 THROTTLE_STOPPED_PWM = 370  # pwm value for no movement
-THROTTLE_REVERSE_PWM = 220  # pwm value for max reverse throttle
+THROTTLE_REVERSE_PWM = 330  # pwm value for max reverse throttle
 #
 # #THROTTLE FOR PIGPIO_PWM
 # THROTTLE_PWM_PIN = 18           #Pin numbering according to Broadcom numbers
