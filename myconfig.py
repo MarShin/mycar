2f2546346595740aac5f340b9ddceb4e6278d01c
MarShin <martinshin95@gmail.com>
2020-07-30 18:33:08 +0800

    change joystick throlttle cap to 1.0

--- myconfig.py
+++ myconfig.py
@@ -29,7 +29,7 @@ IMAGE_H = 224
 # CAMERA_HFLIP = False
 # # For CSIC camera - If the camera is mounted in a rotated position, changing the below parameter will correct the output frame orientation
 CSIC_CAM_GSTREAMER_FLIP_PARM = (
-    0  # (0 => none , 4 => Flip horizontally, 6 => Flip vertically)
+    6  # (0 => none , 4 => Flip horizontally, 6 => Flip vertically)
 )
 #
 # # For IMAGE_LIST camera
@@ -141,7 +141,7 @@ THROTTLE_REVERSE_PWM = 330  # pwm value for max reverse throttle
 #
 # #JOYSTICK
 USE_JOYSTICK_AS_DEFAULT = False  # when starting the manage.py, when True, will not require a --js option to use the joystick
-# JOYSTICK_MAX_THROTTLE = 0.5         #this scalar is multiplied with the -1 to 1 throttle value to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
+JOYSTICK_MAX_THROTTLE = 1.0  # this scalar is multiplied with the -1 to 1 throttle value to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
 # JOYSTICK_STEERING_SCALE = 1.0       #some people want a steering that is less sensitve. This scalar is multiplied with the steering -1 to 1. It can be negative to reverse dir.
 # AUTO_RECORD_ON_THROTTLE = True      #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.
 CONTROLLER_TYPE = "ps4"  # (ps3|ps4|xbox|nimbus|wiiu|F710|rc3|MM1|custom) custom will run the my_joystick.py controller written by the `donkey createjs` command
